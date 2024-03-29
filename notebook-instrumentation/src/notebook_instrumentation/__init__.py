cffda7d2c934ded778505b77dd41cd93a6527fa5
Travis Cline <travis.cline@gmail.com>
2024-03-29 16:03:46 -0700

    n-i: make more robust, get notebook name and hash

--- notebook-instrumentation/src/notebook_instrumentation/__init__.py
+++ notebook-instrumentation/src/notebook_instrumentation/__init__.py
@@ -1,11 +1,18 @@
 import os
-from .instrumentation import getid, instrumentor
+from .instrumentation import (
+    getid, instrumentor,
+    collect_system_context,
+    get_notebook_name,
+    get_notebook_hash,
+)
 
 if os.environ.get("INSTRUMENTATION_DEBUG", None):
     print("🤙 instrumentation is working! 🤙")
 
 def load_ipython_extension(ipython):
-    instrumentor.track(getid(), "notebook_started")
+    instrumentor.track(None, "shell_initialized", {
+        "notebook_name": get_notebook_name(),
+    }, context=collect_system_context(), anonymous_id=getid())
     for e in [
         "shell_initialized",
         "pre_run_cell",
@@ -21,14 +28,14 @@ def extract_info_from_event(event):
     info = {}
     if hasattr(event, "__dict__"):
         for k, v in event.__dict__.items():
-            info[k] = repr(v)
+            info[k] = str(v)
     if hasattr(event, "items"):
         for k, v in event.items():
-            info[k] = repr(v)
+            info[k] = str(v)
     if hasattr(event, "info"):
         iinfo = {}
         for k, v in event.info.__dict__.items():
-            info[k] = repr(v)
+            info[k] = str(v)
         del info["info"]
     return info
 
@@ -36,15 +43,16 @@ def generic_event_handler(event_name, *args):
     event = {}
     properties = {
         "raw_event": repr(event),
+        "notebook_name": get_notebook_name(),
+        "notebook_hash": get_notebook_hash(),
     }
     if len(args) > 0:
-        for k, v in args[0].__dict__.items():
-            properties[k] = repr(v)
-        for k, v in args[0].getattr('info', {}).items():
-            properties[k] = repr(v)
-    instrumentor.track(getid(), event_name, properties)
+        for k, v in extract_info_from_event(args[0]).items():
+            properties[k] = str(v)
+    instrumentor.track(None, event_name, properties, context=collect_system_context(), anonymous_id=getid())
 
 def get_event_handler(event_name):
     def event_handler(*args):
         generic_event_handler(event_name, *args)
     return event_handler
+
--- notebook-instrumentation/src/notebook_instrumentation/instrumentation.py
+++ notebook-instrumentation/src/notebook_instrumentation/instrumentation.py
@@ -1,4 +1,5 @@
 import os
+import json
 import segment.analytics as analytics
 
 analytics.write_key = os.environ.get("S_WRITE_KEY", "")
@@ -12,9 +13,79 @@ analytics.on_error = on_error
 instrumentor = analytics
 
 def getid():
-    '''Returns a machine-specific identifier'''
+    '''Returns a machine-specific anonymous identifier, written and read from ~/.notebook-id'''
+    try:
+        with open(os.path.expanduser("~/.notebook-id"), "r") as f:
+            return f.read().strip()
+    except:
+        import uuid
+        id = str(uuid.uuid1())
+        with open(os.path.expanduser("~/.notebook-id"), "w") as f:
+            f.write(id)
+        return id
+
+
+def get_notebook_path():
+    try:
+        import ipykernel
+        conninfo = json.loads(ipykernel.get_connection_info())
+        return conninfo['jupyter_session']
+    except Exception as e:
+        print(e)
+        return 'unknown'
+
+def get_notebook_name():
+    return os.path.split(get_notebook_path())[-1]
+
+
+def strip_output(nb):
+    for ws in nb.worksheets:
+        for cell in ws.cells:
+            if hasattr(cell, "outputs"):
+                cell.outputs = []
+            if hasattr(cell, "prompt_number"):
+                del cell["prompt_number"]
+
+def strip_notebook_outputs(path):
+    try:
+        from IPython.nbformat.current import read, write
+        nb = open(path, "r").read()
+        return strip_output(nb)
+    except Exception as e:
+        print(e)
+        return None
+
+def get_notebook_hash():
+    try:
+        import hashlib
+        return hashlib.md5(open(get_notebook_path()).read().encode()).hexdigest()
+    except Exception as e:
+        return 'unknown'
+
+
+
+def collect_system_context():
+    '''Collects system context information.'''
     import platform
-    return platform.node()
+    import psutil
+    import os
+
+    properties = {
+        "app": {  # name, version, build.
+            # get notebook information
+            "name": f"notebook {get_notebook_name()}",
+        },
+        "library": {
+            "name": "instrumentation",
+            "version": "0.0.1",
+            "build": "1",
+        },
+        "platform": platform.platform(),
+        "cpu_count": os.cpu_count(),
+        "memory": psutil.virtual_memory(),
+        "disk": psutil.disk_usage("/"),
+    }
+
 
 # TODO:
 # - scoop up account information to .alias
