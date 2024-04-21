e0bfb627b098d1ffaed46fd33ec17f1d34648ec9
Travis Cline <travis.cline@gmail.com>
2024-04-21 15:21:09 -0500

    inshella: Add inshella

---
+++ inshella/go.mod
@@ -0,0 +1,11 @@
+module github.com/tmc/misc/inshella
+
+go 1.22.2
+
+require github.com/tmc/langchaingo v0.1.9
+
+require (
+	github.com/dlclark/regexp2 v1.10.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/pkoukk/tiktoken-go v0.1.6 // indirect
+)
---
+++ inshella/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/tmc/langchaingo/llms"
+	"github.com/tmc/langchaingo/llms/ollama"
+)
+
+var (
+	systemPrompt = `You are a shell programming assistant.
+
+	You will be provided input that represents the task the user is trying to get done and will output the most likely shell command to achieve their goal.`
+)
+
+func main() {
+	llm, err := ollama.New(ollama.WithModel("llama3"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+
+	content := []llms.MessageContent{
+		llms.TextParts(llms.ChatMessageTypeSystem, systemPrompt),
+		llms.TextParts(llms.ChatMessageTypeHuman, strings.Join(os.Args[1:], " ")),
+	}
+	completion, err := llm.GenerateContent(ctx, content, llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
+		fmt.Print(string(chunk))
+		return nil
+	}))
+	if err != nil {
+		log.Fatal(err)
+	}
+	_ = completion
+}
