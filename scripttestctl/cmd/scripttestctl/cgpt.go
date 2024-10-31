23a9e0396cf65268b2ff3a392341237fcbbbeb5f
Travis Cline <travis.cline@gmail.com>
2024-10-31 00:28:29 -0700

    scripttestctl: init

---
+++ scripttestctl/cmd/scripttestctl/cgpt.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+type CGPTIntegration struct{}
+
+func NewCGPTIntegration() *CGPTIntegration {
+	return &CGPTIntegration{}
+}
+
+func (ci *CGPTIntegration) Query(prompt string) (string, error) {
+	cmd := exec.Command("cgpt", "-i", prompt)
+	output, err := cmd.Output()
+	if err != nil {
+		return "", fmt.Errorf("cgpt query failed: %v", err)
+	}
+	return string(output), nil
+}
+
+func (ci *CGPTIntegration) GenerateTest(description string) (string, error) {
+	prompt := fmt.Sprintf("Generate a scripttest for the following description: %s", description)
+	response, err := ci.Query(prompt)
+	if err != nil {
+		return "", err
+	}
+	return extractTestContent(response), nil
+}
+
+func extractTestContent(response string) string {
+	lines := strings.Split(response, "\n")
+	var testContent []string
+	inTestContent := false
+
+	for _, line := range lines {
+		if strings.Contains(line, "```") {
+			inTestContent = !inTestContent
+			continue
+		}
+		if inTestContent {
+			testContent = append(testContent, line)
+		}
+	}
+
+	return strings.Join(testContent, "\n")
+}
---
+++ scripttestctl/go.mod
@@ -0,0 +1,5 @@
+module github.com/yourusername/scripttestctl
+
+go 1.16
+
+require rsc.io/script v0.0.0-20231201171528-c3d9c8f5f4a7
