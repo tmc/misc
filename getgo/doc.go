2a80f3dcef14e9d140254483691f820d7199c123
Travis Cline <travis.cline@gmail.com>
2025-06-06 07:21:04 -0700

    getgo: add getgo package documentation
    Add comprehensive package documentation for getgo tool:
    - Main package documentation in doc.go
    - Additional documentation format in go.doc
    - Describes tool functionality and usage
    - Documents command-line flags and environment variables
    - Includes installation paths and platform support

---
+++ getgo/doc.go
@@ -0,0 +1,47 @@
+```doc.go
+/*
+Package getgo provides functionality to download and install the Go programming language toolchain.
+
+getgo simplifies the process of obtaining and setting up the Go development environment
+by automating the download and installation of the official Go distribution.
+
+Usage:
+
+    getgo install [version]
+
+If no version is specified, the latest stable release will be installed.
+The tool will:
+  - Detect the host operating system and architecture
+  - Download the appropriate Go distribution
+  - Verify the download checksum
+  - Install Go to the standard location for the platform
+  - Configure basic environment variables
+
+Example:
+
+    getgo install 1.20.3
+    getgo install latest
+
+The tool supports common platforms including:
+  - Linux (amd64, arm64, 386)
+  - macOS (amd64, arm64)
+  - Windows (amd64, 386)
+
+Environment Variables:
+
+    GETGO_INSTALL_DIR    Override default installation directory
+    GETGO_NO_VERIFY     Skip checksum verification (not recommended)
+
+The default installation directories are:
+  - Linux/macOS: /usr/local/go
+  - Windows: C:\Go
+
+After installation, users should add the Go binary directory to their PATH:
+  - Linux/macOS: export PATH=$PATH:/usr/local/go/bin
+  - Windows: Add C:\Go\bin to system PATH
+
+For more details see: https://golang.org/doc/install
+
+*/
+package getgo
+```
